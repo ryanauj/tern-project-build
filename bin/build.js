e265a4c6a5469eec0b53cc5b89917aade1181386
Ryan Jackson <ryan@umich.edu>
2017-12-27 13:05:00 -0500

    Moved constants to external file

--- bin/build.js
+++ bin/build.js
@@ -1,72 +1,53 @@
 #! /usr/bin/env node
 
 let fs = require('fs');
-
-const GITIGNORE_FILE = '.gitignore';
-const TERN_PROJECT_FILE = '.tern-project';
-const DEFAULT_NODE_CONTENTS = {
-    'plugins': {
-        'node': {}
-    }
-}
-const DOUBLE_NEWLINE = '\n\n';
-const GITIGNORE_CONTENT = `${DOUBLE_NEWLINE}${TERN_PROJECT_FILE}`;
+let constants = require('./constants.js');
 
 function main() {
-    createDefaultTernProjectFile();
-    updateGitignore();
+    try {
+        createDefaultTernProjectFile();
+        updateGitignore();
+    }
+    catch(err) {
+        print(err);
+    }
 }
 
 function createDefaultTernProjectFile() {
 
+    verifyFileDoesNotExist();
+    createFile();
+
     function verifyFileDoesNotExist() {
-        if (fileExists(TERN_PROJECT_FILE)) {
-            throw `${TERN_PROJECT_FILE} already exists`;
+        if (fileExists(constants.TERN_PROJECT_FILE)) {
+            throw `${constants.TERN_PROJECT_FILE} already exists`;
         }
     }
 
     function createFile() {
-        let contents = prettyStringify(DEFAULT_NODE_CONTENTS);
-        fs.writeFile(TERN_PROJECT_FILE, contents, handleCreateResult);
-    }
-
-    function handleCreateResult(err) {
-        if (err) throw err;
-        log(`${TERN_PROJECT_FILE} has been created!`);
-    }
-
-    try {
-        verifyFileDoesNotExist();
-        createFile();
-    }
-    catch(err) {
-        log(err);
+        let contents = prettyStringify(constants.DEFAULT_NODE_CONTENTS);
+        fs.writeFileSync(constants.TERN_PROJECT_FILE, contents);
+        print(`${constants.TERN_PROJECT_FILE} has been created!`);
     }
 } 
 
 function updateGitignore() {
 
+    verifyFileExists();
+    appendDataToFile();
+
     function verifyFileExists() {
-        if (!fileExists(GITIGNORE_FILE)) {
-            throw `No ${GITIGNORE_FILE} to append to`;
+        if (!fileExists(constants.GITIGNORE_FILE)) {
+            throw `No ${constants.GITIGNORE_FILE} to append to`;
         }
     }
 
-    function appendDataToFile(data, file) {
-        fs.appendFile(GITIGNORE_FILE, GITIGNORE_CONTENT, handleAppendResult);
-    }
-
-    function handleAppendResult(err) {
-        if (err) throw err;
-        log(`${TERN_PROJECT_FILE} was appended to the ${GITIGNORE_FILE}!`);
-    }
-
-    try {
-        verifyFileExists();
-        appendDataToFile();
-    }
-    catch(err) {
-        log(err);
+    function appendDataToFile() {
+        fs.appendFileSync(
+            constants.GITIGNORE_FILE, constants.GITIGNORE_CONTENT);
+        print(constants.TERN_PROJECT_FILE + 
+            ' was appended to the ' + 
+            constants.GITIGNORE_FILE);
     }
 }
 
@@ -84,7 +65,7 @@ function prettyStringify(object) {
     return JSON.stringify(object, null, 4);
 }
 
-function log(message) {
+function print(message) {
     console.log(message);
 }
 
---
+++ bin/constants.js
@@ -0,0 +1,8 @@
+exports.TERN_PROJECT_FILE = '.tern-project',
+exports.TERN_PROJECT_CONTENTS = {
+    'plugins': {
+        'node': {}
+    }
+},
+exports.GITIGNORE_FILE = '.gitignore',
+exports.GITIGNORE_CONTENT = `\n\n${this.TERN_PROJECT_FILE}`
